f6d1fd39a56338582ac45cc33a02e2298653c3e9
DeclanDavis <146258203+DeclanDavis@users.noreply.github.com>
2024-06-04 15:45:33 +0100

    Update RecursionFactorial.java

--- semester-2/RecursionFactorial.java
+++ semester-2/RecursionFactorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class recursionFactorial {
+public class RecursionFactorial {
 	public static void main(String[] args) {
 	
 	Scanner sc = new Scanner(System.in);
