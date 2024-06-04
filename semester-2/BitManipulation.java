1039a5bf2ed012093b91b327040a5199e641648d
DeclanDavis <146258203+DeclanDavis@users.noreply.github.com>
2024-06-04 20:01:50 +0100

    Update BitManipulation.java

--- semester-2/BitManipulation.java
+++ semester-2/BitManipulation.java
@@ -1,7 +1,17 @@
 public class BitManipulation {
-
 	public static void main(String[] args) {
+	
+	/*
+	Prove these through pen and paper calculation
+	*/
+	System.out.println("Q1: ");
+	System.out.println(((32 | 13 ) >> 2 ) & 9 );
+
+	System.out.println("Q2: ");
+	System.out.println(5|7|16|11) & 167) >> 1);
+
+	System.out.println("Q3: ");
+	System.out.println((((11 & 19)|5) << 3);
 		
-		System.out.println(((43 & 27 ) << 3 ) | ( 14 | 9 ));
 	}
 }
